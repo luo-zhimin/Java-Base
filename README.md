2ca8593b9a3652c5c9ccf1f4d227ec44763f9e78
罗志敏 <1798677862@qq.com>
2022-07-29 15:50:40 +0800

    mysql string function submit

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Java-Base
 
-java基础 变量、结构控制、多线程、项目、file、socket....
+java基础 变量、结构控制、多线程、集合、泛型、反射、file、socket、mysql、项目(一个大阶段一个)....
 
-## 推荐
+* 推荐
 
-1.推荐看file + socket 实现了发送离线发送文件 私聊 新闻推送等 使用了socket(tcp)+多线程+oop，在socketProject下
-
-2.
+  > 1.(多用户即时通讯系统) file + socket 实现了发送离线发送文件 私聊 新闻推送等 使用了socket(tcp)+多线程+oop，在socketProject下
 
 # 笔记分享
 
-语雀地址： https://www.yuque.com/books/share/2401b162-4e19-4a11-9c26-c44507206b3a?# 《后端》
+> 语雀地址： https://www.yuque.com/books/share/2401b162-4e19-4a11-9c26-c44507206b3a?# 《后端》
+
+[//]: # (<img src='' alt="路线图"/>)
--- src/main/resources/msqyl/mysql-function.sql
+++ src/main/resources/msqyl/mysql-function.sql
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 -- FUNCTION 函数
 
 -- 合计/统计函数
@@ -58,14 +66,6 @@ FROM emp
 GROUP BY deptno
 ORDER BY deptno;
 
-/*
- * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 -- 显示每个部门的每种岗位的平均工资和最低工资?
 
 SELECT AVG(sal), min(sal), deptno, job
@@ -83,7 +83,6 @@ HAVING avg_sal < 2000;
 -- WHERE是一个约束声明，在查询数据库的结果返回之前对数据库中的查询条件进行约束，即在结果返回之前起作用，且where后面不能使用“聚合函数”，因为where的执行顺序在聚合函数之前。
 -- HAVING是一个过滤声明，过滤是在查询数据库的结果返回之后进行过滤，即在结果返回之后起作用，且having后面可以使用“聚合函数”。注意：having是对查出来的结果进行过滤，那么对没有查出来的值就不能使用having
 
-
 SELECT *
 from dept;
 SELECT *
@@ -91,6 +90,74 @@ from emp;
 SELECT *
 from salgrade;
 
+-- 字符串函数
+# 详情
+# CHARSET(str) 返回字串字符集
+select charset(deptno)
+from emp;
+-- utf8mb4
+# CONCAT (string2 [,... ]) 连接字串  多个字符拼接一列
+select concat(ename, ' job is ', job)
+from emp;
+-- sdasda1
+# INSTR(string ,substring) 返回substring在string中出现的位置，没有返回0
+select instr('adwad', 'w')
+from dual;
+-- dual 虚拟表 系统的 可以作为一个测试表使用
+# UCASE (string2) 转换成大写
+select ucase(ename)
+from emp;
+-- AAAW11223Z
+# LCASE (string2) 转换成小写
+select lcase(ename)
+from emp;
+-- waaasda1
+# LEFT (string2 ,length ) 从string2中的左边起取length个字符
+select left(ename, 2) target, ename source
+from emp;
+# right 从string2 中的右边起取length个字符
+select right(ename, 3)
+from emp;
+# LENGTH (string)string长度【按照字节】
+select length(ename) length, ename
+from emp;
+# REPLACE (str search_str ,replace_str ) 在str中用replace_str替换search_str
+select lcase(ename) as ename, replace(job, 'MANAGER', '经理') replaceTarget
+from emp;
+# STRCMP (string1 ,string2) 逐字符比较两字串大小 返回 -1 0 1 -> {>,=,<}
+select strcmp(ename, empno)
+from emp;
+# SUBSTRING (str,position [,length ]) 从str的position开始【从1开始计算】，取length 个字符
+select substring(ename, 1, 2), ename
+from emp;
+-- 从第一个位置开始截取 截取2位
+# LTRIM(string2) 去除前端空格  RTRIM(string2) 后端空格 trim 去除俩边的空格
+select ltrim(' sss ss'), rtrim(' www '), trim(' sdas ')
+from dual;
+select *
+from emp;
 
+# 以首字母小写的方式显示所有员工 emp 表的姓名
+# replace + substring
+# first
+select replace(ename, substring(ename, 1, 1), substring(lcase(ename), 1, 1))
+from emp;
+# second
+select replace(ename, left(ename, 1), lcase(left(ename, 1)))
+from emp;
+select concat(lcase(substring(ename, 1, 1)), substring(ename, 2)) as newName
+from emp;
+select concat(lcase(left(ename, 1)), right(ename, (length(ename) - 1)))
+from emp;
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+-- question ONLY_FULL_GROUP_BY 问题 临时修改
 -- SELECT @@sql_mode;
 -- set sql_mode ='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
