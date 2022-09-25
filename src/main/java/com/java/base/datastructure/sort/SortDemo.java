5f2c2daee20deacc1c5b130e22ecdf7c1d7301ec
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-09-25 21:32:42 +0800

    复杂度-[时间频度]时间/空间

---
+++ src/main/java/com/java/base/datastructure/sort/SortDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) luoZhiMin 2022.9.25.1.2.28
+ */
+
+package com.java.base.datastructure.sort;
+
+/**
+ * Created by IntelliJ IDEA.
+ * 排序介绍
+ * @Author : 镜像
+ * @create 2022/9/25 13:02
+ */
+public class SortDemo {
+
+    /*
+        内部排序: 指将需要处理的所有数据都加载到内部存储器(内存)中进行排序。
+        外部排序法：数据量过大，无法全部加载到内存中，需要借助外部存储(文件等)进行排序
+     */
+
+    /**
+     * <img src="https://cdn.nlark.com/yuque/0/2022/png/27601787/1664082219766-a636084f-dc8e-4e50-8ffd-a42b42fb15cb.png">
+     */
+    private void sortGroup(){}
+}
