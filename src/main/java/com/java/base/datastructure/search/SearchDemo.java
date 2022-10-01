4113de7cc8e507611c1e03536720947a10d680c0
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-10-01 23:23:38 +0800

    查找开始

---
+++ src/main/java/com/java/base/datastructure/search/SearchDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) luoZhiMin 2022.9.30.7.37.35
+ */
+
+package com.java.base.datastructure.search;
+
+/**
+ * Created by IntelliJ IDEA.
+ * 查找算法 - 四种
+ * @Author : 镜像
+ * @create 2022/9/30 19:37
+ */
+public class SearchDemo {
+
+    /*
+        顺序(线性)查找
+        二分查找/折半查找
+        插值查找
+        斐波那契查找
+     */
+
+
+}
